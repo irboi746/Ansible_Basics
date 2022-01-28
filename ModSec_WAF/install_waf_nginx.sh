1e71c243f80b2576bc6a4918631ad1340cda4af7
irboi746 <88809233+irboi746@users.noreply.github.com>
2022-01-28 14:33:29 +0800

    Add files via upload

--- ModSec_WAF/install_waf_nginx.sh
+++ ModSec_WAF/install_waf_nginx.sh
@@ -3,10 +3,15 @@
 # logs are located in /var/log/modsec_audit.log
 # !!! Run as root
 
+#Debug Trap
+set -e 
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "\${last_command}\" command filed with exit code $?."' EXIT
+
 # Install Dependencies
 apt update && apt install wget git make gcc build-essential autoconf automake libtool libfuzzy-dev ssdeep gettext pkg-config libcurl4-openssl-dev liblua5.3-dev libpcre3 libpcre3-dev libxml2 libxml2-dev libyajl-dev doxygen libcurl4 libgeoip-dev libssl-dev zlib1g-dev libxslt-dev liblmdb-dev libpcre++-dev libgd-dev -y
 
-# Download and Build ModSecurity
+# Download and Build ModSecurity (works)
 cd /opt && git clone https://github.com/SpiderLabs/ModSecurity && cd ModSecurity
 git submodule init && git submodule update
 ./build.sh && ./configure && make && make install
@@ -16,12 +21,12 @@ cd /opt && git clone --depth 1 https://github.com/SpiderLabs/ModSecurity-nginx.g
 nginxvnumber=$(nginx -v 2>&1 | grep -o '[0-9.]*')
 cd /opt && wget http://nginx.org/download/nginx-"$nginxvnumber".tar.gz && tar zxvf nginx-"$nginxvnumber".tar.gz
 cd nginx-"$nginxvnumber"
-./configure --with-compat --add-dynamic-module=/opt/ModSecurity-nginx && make modules && cp objs/ngx_http_modsecurity_module.so /etc/nginx/modules
+./configure --with-compat --add-dynamic-module=/opt/ModSecurity-nginx && make modules && cp objs/"ngx_http_modsecurity_module.so" /etc/nginx/modules-available/"ngx_http_modsecurity_module.so"
 nginx -t
 
 # Load Module into nginx.conf (works)
 cp /etc/nginx/nginx.conf ~/nginx.conf
-sed -i -e '1iload_module modules/ngx_http_modsecurity_module.so;\' ~/nginx.conf
+sed -i -e '1iload_module /etc/nginx/modules-available/"ngx_http_modsecurity_module.so";\' ~/nginx.conf
 cp -f ~/nginx.conf /etc/nginx/nginx.conf
 rm -rf ~/nginx.conf
 
@@ -41,7 +46,6 @@ mv /usr/share/modsecurity-crs/rules/REQUEST-900-EXCLUSION-RULES-BEFORE-CRS.conf.
 echo -e "# From https://github.com/SpiderLabs/ModSecurity/blob/master/\n# modsecurity.conf-recommended\n#\n# Edit to set SecRuleEngine On\nInclude \"/etc/nginx/modsec/modsecurity.conf\"\nInclude \"/usr/share/modsecurity-crs/crs-setup.conf\"\nInclude \"/usr/share/modsecurity-crs/rules/*.conf\"\n" > /etc/nginx/modsec/main.conf
 
 # Enable modsecurity for all sites (works)
-# Enable modsecurity for all sites
 for file in /etc/nginx/sites-available/*; do
      sed '/^server.*/a \\tmodsecurity on;\n\tmodsecurity_rules_file /etc/nginx/modsec/main.conf;\n' "${file}" > "${file}1"
      rm "${file}"
